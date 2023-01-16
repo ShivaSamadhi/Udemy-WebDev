812a677055f16f924597ee979119aac82b4ea9d7
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-16 15:51:04 -0600

    Functions Exercises 2

--- ES6-Review.js
+++ ES6-Review.js
@@ -125,19 +125,60 @@ const changeCase = (name) => {
 
     console.log(finalName)
 }
-changeCase()
+changeCase();
 
 /*Functions*/
-const milkPrice = 5.76
-const money = 50
+const milkPrice = 5.76;
+const money = 40;
 
 const getMilk = (money, milkPrice) => {
-    console.log(quantityPurchased(money, milkPrice))
-    console.log(exactChange(money, milkPrice))
+    console.log(quantityPurchased(money, milkPrice));
+    console.log(exactChange(money, milkPrice));
 }
 
-const quantityPurchased = (money, milkPrice) => Math.floor(money/milkPrice)
+const quantityPurchased = (money, milkPrice) => Math.floor(money/milkPrice);
 
-const exactChange = (money, milkPrice) => (money - (milkPrice * quantityPurchased(money, milkPrice))).toFixed(2)
+const exactChange = (money, milkPrice) => (money - (milkPrice * quantityPurchased(money, milkPrice)))
+    .toFixed(2);
 
-getMilk(money, milkPrice)
+getMilk(money, milkPrice);
+
+/*In this challenge, you are going to create a function that tells us how many days, weeks and months we have left if we live until 90 years old.
+
+It will take your current age as the input and console.logs a message with our time left in this format:
+
+You have x days, y weeks, and z months left.
+Where x, y and z are replaced with the actual calculated numbers.
+
+For this challenge, assume there are 365 days in a year, 52 weeks in a year and 12 months in a year.
+
+IMPORTANT your console.log output should match the Example Output format exactly, even the positions of the commas and full stops.
+
+Example Input
+e.g. If you are 56 years old:
+lifeInWeeks(56)
+Example Output
+You have 12410 days, 1768 weeks, and 408 months left.
+
+Hint
+Try using this Repl.it playground to test your code and see if the output is what you expect it to be.
+Make sure your console.log output matches the example output precisely. The same capitalisation, the same spaces, commas and full stops.*/
+const YearConversion = (years, conversionRate) => years * conversionRate
+const lifeInWeeks = (age) => {
+
+    let daysPerYear = 365
+    let weeksPerYear = 52
+    let monthsPerYear = 12
+
+    let totalLifespan = 90;
+    let remainingLifespan = totalLifespan - age;
+
+    let daysRemaining = remainingLifespan * daysPerYear
+    let weeksRemaining = remainingLifespan * weeksPerYear
+    let monthsRemaining = remainingLifespan * monthsPerYear
+
+    let timeLeftInLifespans = `You have ${daysRemaining} days, ${weeksRemaining} weeks, and ${monthsRemaining} months left`
+
+    console.log(timeLeftInLifespans)
+}
+lifeInWeeks(51)
