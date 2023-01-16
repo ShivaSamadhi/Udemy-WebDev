13353b78ae7f6eed142ce429cf85ff58939134d2
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-16 10:30:11 -0600

    Solution Explanation: Destructuring Array Syntax

--- ES6-Review.js
+++ ES6-Review.js
@@ -27,3 +27,30 @@ const test = () => {
 }
  test()
 
+/*
+Destructuring assignment (a feature of ES2015) lets you extract items of an array into variables. For example, the following code destructures an array:
+
+let a;
+let b;
+[a, b] = [1, 2, 3];
+a; // => 1
+b; // => 2
+[a, b] = [1, 2, 3] is a destructuring assignment that destructures [1, 2, 3] array. a variable is assigned with the first item 1 of [1, 2, 3], correspondingly b is assigned with the second item 2.
+
+Knowing how to destructure an array, it's easy to use it for swapping variables. Let's swap the variables a and b using destructuring assignment:
+
+let a = 1;
+let b = 2;
+[a, b] = [b, a];
+a; // => 2
+b; // => 1
+[a, b] = [b, a] is the destructuring assignment that swaps the variables a and b.
+
+At the first step, on the right side of the destructuring, a temporary array [b, a] (which evaluates to [2, 1]) is created.
+
+Then the destructuring of the temporary array occurs: [a, b] = [2, 1]. The variable a is assigned with 2, and b with 1. The swapping of a and b has been performed.
+
+I like the destructuring approach because it's short and expressive: swapping is performed in just one statement. It works with any data type: numbers, strings, booleans, objects.
+
+I recommend swapping variables using a destructuring assignment for most of the cases.
+ */
