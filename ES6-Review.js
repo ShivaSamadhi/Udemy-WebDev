2530938b3dc9dceb56456f368ec724775d2dd532
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-17 11:25:42 -0600

    Intermediate JS Exercises

--- ES6-Review.js
+++ ES6-Review.js
@@ -287,6 +287,14 @@ const fizzBuzz = (maxValue) => {
 }
 console.log(fizzBuzz(15))
 
+/*
+* You are going to write a function which will select a random name from a list of names. The person selected will have to pay for everybody's food bill.
+Important: The output should e returned from the function and you do not need alert, prompt or console.log. The output should match the example output exactly, including capitalisation and punctuation.
+Example Input
+["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+Example Output
+Michael is going to buy lunch today!
+*/
 const whosPaying = () => {
     const guestList = ["Ramaj", "Anthony", "Miguel", "Andrew", "Johnson"]
 
