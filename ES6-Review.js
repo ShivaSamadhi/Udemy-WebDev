93fc2bababff2edb01674fba698aeef1ed8a0e6d
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-17 09:27:52 -0600

    Intermediate JS Exercises

--- ES6-Review.js
+++ ES6-Review.js
@@ -196,9 +196,29 @@ The first parameter should be the weight and the second should be the height.
 NOTE: You do not need to write any alerts or prompts or console logs. Your code should only contain the function, the result has to be returned by the function. You do not need to call the function.
 */
 
-const bmiCalculator = (weight, height) => Math.round(weight/height**2)
+const bmiCalculator = (weight, height) => {
+    const bmiScore = Math.round(weight/height**2)
 
-console.log(bmiCalculator(65, 1.8))
+/*
+Previously, we've created a function that is able to calculate the BMI. But once we get a result, we will want to tell the user what the number means.
+Write a function that outputs (returns) a different message depending on the BMI.
+
+BMI <18.5, the output should be: "Your BMI is <bmi>, so you are underweight."
+BMI 18.5-24.9, the output should be "Your BMI is <bmi>, so you have a normal weight."
+BMI >24.9, the output should be "Your BMI is <bmi>, so you are overweight."
+*/
+
+    if(bmiScore > 24.9)
+        return `Your BMI is ${bmiScore}, so you are overweight.`
+
+    if(bmiScore >= 18.5 && bmiScore <= 24.9)
+        return `Your BMI is ${bmiScore}, so you are a normal weight.`
+
+    else
+        return `Your BMI is ${bmiScore}, so you are underweight.`
+}
+
+console.log(bmiCalculator(35, 1.8))
 
 /*Random Number Generation*/
 const loveCalculator = () => {
