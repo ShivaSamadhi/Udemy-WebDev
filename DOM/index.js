6d5bb6e110f1728ef79966fb2636b82e41e925c2
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-17 14:40:40 -0600

    DOM Manipulation Exercises

--- DOM/index.js
+++ DOM/index.js
@@ -1,11 +1,22 @@
 "use strict"
 
 
-const thirdLi = (
-                    document
+const thirdLi = document
                     .querySelector("#list li:nth-child(3)")
                     .innerHTML = `Ramaj`
-                )
-const linkColor = (
-    document.querySelector("#list .list a").style.color = 'red'
-)
+
+const linkColor = document
+    .querySelector("#list .list a")
+    .style
+    .color = 'red'
+
+
+const buttonColor = document
+    .querySelector("#btn")
+    .style
+    .backgroundColor = "yellow"
+
+
+const buttonVisibility = document.querySelector("#btn").classList.add("invisible")
+
+console.log(document.querySelector('#btn').classList)
