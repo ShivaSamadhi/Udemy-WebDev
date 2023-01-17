d7d579c8dd2e16a6bfd2f007cde0edda31ede36d
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-17 09:40:30 -0600

    Intermediate JS Exercises

--- ES6-Review.js
+++ ES6-Review.js
@@ -226,3 +226,27 @@ const loveCalculator = () => {
     console.log(loveScore)
 }
 loveCalculator()
+
+/*
+Write a program that works out whether if a given year is a leap year. A normal year has 365 days, leap years have 366, with an extra day in February. The reason why we have leap years is really fascinating, this video goes into more detail.
+This is how to work out whether if a particular year is a leap year:
+A year is a leap year if it is evenly divisible by 4 ;
+except if that year is also evenly divisible by 100;
+unless that year is also evenly divisible by 400.
+
+e.g. Is the year 2000 a leap year?:
+2000 ÷ 4 = 500 (Leap)
+2000 ÷ 100 = 20 (Not Leap)
+2000 ÷ 400 = 5 (Leap!)
+
+*/
+const leapYearCalculator = (year) => {
+    if (year%4 === 0 && year%100 !== 0)
+        return `${year} is a leap year.`
+    if (year%400 === 0)
+        return `${year} is a leap year.`
+    else
+        return `${year} is not a leap year.`
+
+}
+console.log(leapYearCalculator(2100))
