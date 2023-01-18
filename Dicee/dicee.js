02267a60535b4169218a378300befb49e995f4ce
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-18 07:35:41 -0600

    Dicee Challenge

--- Dicee/dicee.js
+++ Dicee/dicee.js
@@ -1,1 +1,13 @@
 "use strict"
+
+const randomNumP1 = () => {
+  const numGenerator = Math.floor((Math.random()*6)+1);
+    console.log(numGenerator)
+    return numGenerator
+}
+
+const randomNumP2 = () => {
+    const numGenerator = Math.floor((Math.random()*6)+1);
+    console.log(numGenerator)
+    return numGenerator
+}
