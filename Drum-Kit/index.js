7c4e26734aadd3817117d12a25d2721315ba1a96
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-19 11:10:49 -0600

    DOM Drum Kit

--- Drum-Kit/index.js
+++ Drum-Kit/index.js
@@ -1,7 +1,30 @@
 "use strict"
 
 const drumBtns = document.querySelectorAll(".drum");
-const playSound = () => {
+const playSound = (instrument) => {
+    let audio
+    switch (instrument) {
+        case `w`:
+            audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+            break;
+
+        case `a`:
+            audio = new Audio('sounds/tom-2.mp3')
+            audio.play();
+            break;
+
+        case `s`:
+            audio = new Audio('sounds/tom-3.mp3')
+            audio.play();
+            break;
+
+        case `a`:
+            audio = new Audio('sounds/tom-2.mp3')
+            audio.play();
+            break;
+
+    }
     // const audio = new Audio('sounds/tom-1.mp3')
     // audio.play();
     }
@@ -10,6 +33,6 @@ for (const drumBtn of drumBtns) {
     const instrument = drumBtn.textContent
     console.log(instrument)
     drumBtn.addEventListener("click", () => {
-        playSound()
+        playSound(instrument)
     })
 }
