d75a42cbda5b938b040461b9ee0cff2b78e7a560
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-19 14:12:55 -0600

    jQuery Exercises

--- jQuery/index.js
+++ jQuery/index.js
@@ -1,6 +1,8 @@
 "use strict"
 //Make sure to check that the document is ready to run. This can be done with the function below but best practice is to place all scripts at the bottom of the body section so all HTML elements load first.
 
-$(document).ready(()=>{
-    $('h1').css("color", "red")
-})
+// $(document).ready(()=>{
+//     $('h1').css("color", "red")
+// })
+
+$('h1').addClass('big-title')
