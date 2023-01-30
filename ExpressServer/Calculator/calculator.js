65ef16f430e9af74950817ee90e46d43d974b917
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-30 11:17:38 -0600

    Express JS

--- ExpressServer/Calculator/calculator.js
+++ ExpressServer/Calculator/calculator.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
+//Handles the GET reqs at the specified route
 app.get("/", (req, res) => {
-    res.send(`<h1>Hello World</h1>`)
+    //Once the req is received, a specific file can be sent back to the client as the res using __dirname to create an absolute path to the file
+    res.sendFile(`${__dirname}/index.html`)
 })
 
+// Creates a request listener for the Port specified and runs the associated callback function
 app.listen(3000, () => {
     console.log("Server Started: Port 3000")
 });
