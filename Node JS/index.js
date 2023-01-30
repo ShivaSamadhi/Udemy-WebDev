86e98e803525960d16553acfd67ca00e9fbd0dfc
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-30 10:17:27 -0600

    Node JS

--- Node JS/index.js
+++ Node JS/index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const superheroes = require("superheroes");
 const supervillain = require("supervillains")
 let myHeroName = superheroes.random()
-let myVillainName = superheroes.random()
+let myVillainName = supervillain.random()
 
 console.log(`Hero Name: ${myHeroName}`)
 console.log(`Villain Name: ${myVillainName}`)
