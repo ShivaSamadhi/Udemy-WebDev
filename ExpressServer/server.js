863afe9d005ca4cf5cfc14f51923d168b73d16eb
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-30 10:41:31 -0600

    Express JS

--- ExpressServer/server.js
+++ ExpressServer/server.js
@@ -1,4 +1,10 @@
 const express = require('express');
 const app = express();
 
-app.listen(3000);
+app.get("/", (req, res) => {
+    res.send(`hello`)
+})
+
+app.listen(3000, () => {
+    console.log("Server Started: Port 3000")
+});
