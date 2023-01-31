df6b05a2432e1fa8a6c9890b6787ad9560cd8531
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-31 13:54:44 -0600

    API Practice

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -5,7 +5,7 @@ const {response} = require("express");
 
 const app = express();
 
-const weatherMap = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&units=imperial&appid=df70112efc82a0e3f98ed3fe21df78b5`
+const weatherMap = `https://api.openweathermap.org/data/2.5/forecast?lat=32.7831&lon=-96.8067&units=imperial&appid=df70112efc82a0e3f98ed3fe21df78b5`
 
 
 app.get(`/`, (req, res) =>{
@@ -16,9 +16,13 @@ app.get(`/`, (req, res) =>{
         response.on(`data`, (data)=>{
             const weatherData = JSON.parse(data)
             const {list: [{main: {temp}, weather: [{description}]}]} = weatherData
+
             console.log(temp)
             console.log(description)
 
+            res.write(`<h1>Dallas Temp: ${temp}ºF</h1>`)
+            res.write(`<p>The weather is currently: ${description}</p>`)
+            res.send
         })
     })
 
