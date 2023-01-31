86ac1d5a7db9db050ff7f8d247000df757455a74
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-31 16:06:23 -0600

    API Practice

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -9,9 +9,11 @@ const app = express();
 const weatherMap = `https://api.openweathermap.org/data/2.5/forecast`
 const lat = `lat=32.7831`
 const lon= `lon=-96.8067`
+const cityName = ``
 const units = `units=imperial`
 const apiKey = `appid=df70112efc82a0e3f98ed3fe21df78b5`
 const url = `${weatherMap}?${lat}&${lon}&${units}&${apiKey}`
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 
@@ -26,23 +28,27 @@ app.post(`/`, (req, res) => {
 
     const {cityName} = request
     console.log(cityName)
+
+    const cityUrl = `${weatherMap}?p=${cityName}&${units}&${apiKey}`
+
+    https.get(cityUrl, (response) =>{
+    console.log(response.statusCode)
+
+    response.on(`data`, (data)=>{
+        const weatherData = JSON.parse(data)
+        const {list: [{main: {temp}, weather: [{description}]}]} = weatherData
+
+        console.log(temp)
+        console.log(description)
+
+        res.write(`<h1>${cityName} Temp: ${temp}F</h1>`)
+        res.write(`<p>The weather is currently: ${description}</p>`)
+        res.send()
+    })
+})
 })
 
-// https.get(url, (response) =>{
-//     console.log(response.statusCode)
-//
-//     response.on(`data`, (data)=>{
-//         const weatherData = JSON.parse(data)
-//         const {list: [{main: {temp}, weather: [{description}]}]} = weatherData
-//
-//         console.log(temp)
-//         console.log(description)
-//
-//         res.write(`<h1>Dallas Temp: ${temp}F</h1>`)
-//         res.write(`<p>The weather is currently: ${description}</p>`)
-//         res.send()
-//     })
-// })
+
 
 
 
