5a3eeabc8f2a6564cdfaad209f4472514ddeae0e
Ramaj Johnson <rjohnson3795@gmail.com>
2023-01-31 12:48:52 -0600

    API Practice

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,10 +1,20 @@
+
 const express = require('express');
 const https = require('https');
+const {response} = require("express");
 
 const app = express();
 
+const weatherMap = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=`
+const API_TOKEN = `df70112efc82a0e3f98ed3fe21df78b5`
+
 app.get(`/`, (req, res) =>{
-    https.get()
+
+    const url = `${weatherMap}${API_TOKEN}`
+
+    https.get(url, (response) =>{
+        console.log(response)
+    })
 
 
     res.send("Server: ACTIVE")
