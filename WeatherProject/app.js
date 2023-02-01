9e6f2c4fbbed6a7621c89b003fe2ec51dd355547
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-01 11:51:21 -0600

    API Refactor

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -14,9 +14,28 @@ const units = `units=imperial`
 const apiKey = `appid=df70112efc82a0e3f98ed3fe21df78b5`
 const url = `${weatherMap}?${lat}&${lon}&${units}&${apiKey}`
 
+const weatherAPIReq = (url) => {
+    https.get(url, (response) =>{
+        console.log(response.statusCode)
+
+        response.on(`data`, (data)=>{
+            const weatherData = JSON.parse(data)
+            const {list: [{main: {temp}, weather: [{description}]}]} = weatherData
+
+            console.log(temp)
+            //console.log(description)
+
+            res.write(`<h1>${cityName} Temp: ${temp}F</h1>`)
+            res.write(`<p>The weather is currently: ${description}</p>`)
+            res.send()
+        })
+    })
+}
+
 
 app.use(bodyParser.urlencoded({extended: true}))
 
+//HTTPS Requests
 app.get(`/`, (req, res) =>{
 
    res.sendFile(`${__dirname}/index.html`)
@@ -31,21 +50,7 @@ app.post(`/`, (req, res) => {
 
     const cityUrl = `${weatherMap}?q=${cityName}&${units}&${apiKey}`
 
-    https.get(cityUrl, (response) =>{
-    console.log(response.statusCode)
-
-    response.on(`data`, (data)=>{
-        const weatherData = JSON.parse(data)
-        const {list: [{main: {temp}, weather: [{description}]}]} = weatherData
-
-        console.log(temp)
-        //console.log(description)
-
-        res.write(`<h1>${cityName} Temp: ${temp}F</h1>`)
-        res.write(`<p>The weather is currently: ${description}</p>`)
-        res.send()
-        })
-    })
+    weatherAPIReq(cityUrl)
 })
 
 
