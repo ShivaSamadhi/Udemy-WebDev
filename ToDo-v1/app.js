6cda8d7bee653c863559df4a121a3f6a912a344e
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-02 11:36:04 -0600

    ToDo App

---
+++ ToDo-v1/app.js
@@ -0,0 +1,10 @@
+//Required Modules
+const express = require(`express`);
+const https = require('https');
+const bodyParser = require(`body-parser`);
+const {request, response} = require(`express`)
+
+//Express App
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}))
