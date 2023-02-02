bba6366c9a4b62d64aebc0a58de823f65e4b2bc1
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-02 11:41:13 -0600

    ToDo App

--- ToDo-v1/app.js
+++ ToDo-v1/app.js
@@ -8,3 +8,13 @@ const {request, response} = require(`express`)
 const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}))
+
+//Requests
+app.get(`/`, (req, res) => {
+    res.send(`Hello`)
+})
+
+//Port Listener
+app.listen(8080, () => {
+    console.log("Server Started: Port 8080")
+});
