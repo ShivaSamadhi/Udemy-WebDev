94eaeeef54c331790f57fc9f44aaf4a631351cf8
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-03 10:45:28 -0600

    EJS Templating

--- ToDo-v1/app.js
+++ ToDo-v1/app.js
@@ -14,36 +14,17 @@ app.use(bodyParser.urlencoded({extended: true}))
 //Requests
 app.get(`/`, (req, res) => {
     let today = new Date();
-    let currentDay = today.getDay()
-    let day = ''
 
-    switch(currentDay){
-        case 0:
-            day = `Sunday`
-            break;
-        case 1:
-            day = `Monday`
-            break;
-        case 2:
-            day = `Tuesday`
-            break;
-        case 3:
-            day = `Wednesday`
-            break;
-        case 4:
-            day = `Thursday`
-            break;
-        case 5:
-            day = `Friday`
-            break;
-        case 6:
-            day = `Saturday`
-            break;
-        default:
-            console.log(`error`)
+    const dateOptions = {
+        weekday: `long`,
+        day: `numeric`,
+        month: `short`
     }
 
-    res.render(`list`, {day: day})
+    let currentDay = today.toLocaleDateString(`en-US`, dateOptions)
+
+
+    res.render(`list`, {currentDay: currentDay})
 
 })
 
