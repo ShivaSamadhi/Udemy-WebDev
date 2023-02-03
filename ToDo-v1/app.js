746f25c40214c3e77b7e371e7d951556b31cfc80
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-03 10:25:15 -0600

    EJS Templating

--- ToDo-v1/app.js
+++ ToDo-v1/app.js
@@ -15,14 +15,33 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.get(`/`, (req, res) => {
     let today = new Date();
     let currentDay = today.getDay()
-    let dayMessage = ''
+    let day = ''
 
-    if(currentDay === 6 || currentDay === 0)
-        dayMessage = `the WEEKEND!`
-    else
-        dayMessage = `a WEEKDAY!`
+    switch(currentDay){
+        case 0:
+            day = `Sunday`
+            break;
+        case 1:
+            day = `Monday`
+            break;
+        case 2:
+            day = `Tuesday`
+            break;
+        case 3:
+            day = `Wednesday`
+            break;
+        case 4:
+            day = `Thursday`
+            break;
+        case 5:
+            day = `Friday`
+            break;
+        case 6:
+            day = `Saturday`
+            break;
+    }
 
-    res.render(`list`, {kindOfDay: dayMessage})
+    res.render(`list`, {day: day})
 
 })
 
