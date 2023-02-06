774a509d2c39bd151178618b90119028d5b063c0
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-06 11:16:18 -0600

    Advanced JS Review

--- DOM/practice.js
+++ DOM/practice.js
@@ -2,16 +2,22 @@
 
 let paraLink = document.querySelector(`p > a`);
 let h1 = document.querySelector(`h1`)
+let bodyElem = document.querySelector(`body`)
 
+let newPara = document.createElement(`p`)
 
 console.log(paraLink)
-
-
-
 paraLink.setAttribute(`href`, `https://www.youtube.com`)
+console.log(paraLink.getAttribute(`href`))
+
 h1.setAttribute(`id`, `h1`)
 
 let h1ID = document.querySelector(`#h1`)
 h1ID.style.color = `#ae53ea`
 
-console.log(paraLink.getAttribute(`href`))
+bodyElem.appendChild(newPara)
+console.log(bodyElem)
+
+let para2 = document.querySelector(`p:nth-of-type(2)`)
+console.log(para2)
+
