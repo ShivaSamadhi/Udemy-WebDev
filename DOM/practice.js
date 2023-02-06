6b7e3b82546b5cd5d3a9ab3b5c3c6a96b14af349
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-06 11:44:38 -0600

    Advanced JS Review

--- DOM/practice.js
+++ DOM/practice.js
@@ -15,7 +15,7 @@ h1.setAttribute(`id`, `h1`)
 let h1ID = document.querySelector(`#h1`)
 h1ID.style.color = `#ae53ea`
 
-bodyElem.appendChild(newPara)
+bodyElem.append(newPara)
 console.log(bodyElem)
 
 let para2 = document.querySelector(`p:nth-of-type(2)`)
@@ -24,3 +24,10 @@ para2.setAttribute(`class`, `para2`)
 para2.textContent = `Hello`
 let para2Class = para2.getAttribute(`class`)
 console.log(para2Class)
+
+//Input Event Listener
+let textInput = document.querySelector(`#textInput`)
+const strCount = () => {
+
+}
+textInput.addEventListener(`keydown`, ())
