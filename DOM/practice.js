aa1c7e75a35e9a0ef98e30e6fad5224e476c305c
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-06 11:03:29 -0600

    Advanced JS Review

--- DOM/practice.js
+++ DOM/practice.js
@@ -1,11 +1,17 @@
 "use strict"
 
 let paraLink = document.querySelector(`p > a`);
+let h1 = document.querySelector(`h1`)
+
 
 console.log(paraLink)
 
 
 
 paraLink.setAttribute(`href`, `https://www.youtube.com`)
+h1.setAttribute(`id`, `h1`)
+
+let h1ID = document.querySelector(`#h1`)
+h1ID.style.color = `#ae53ea`
 
 console.log(paraLink.getAttribute(`href`))
