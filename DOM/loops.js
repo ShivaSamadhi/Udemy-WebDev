e2ecaee254843f4fbb330fcf0e348f5508f19a68
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-06 15:03:35 -0600

    Advanced JS Review

--- DOM/loops.js
+++ DOM/loops.js
@@ -1,3 +1,22 @@
-for (let i = 0; i < 10; i++) {
+"use strict"
 
+const submitBtn = document.querySelector(`#subBtn1`)
+const forLoopDiv = document.querySelector(`#forLoops`)
+
+
+
+const addingLoop = () => {
+  const numInput = document.querySelector(`#userNum`).value;
+
+  const sumElement = document.createElement(`h6`)
+
+  let count = 0
+
+  for (let i = 0; i <= numInput; i++) {
+        count += i
+    }
+
+  sumElement.textContent =`The sum of all numbers leading up to ${numInput} is ${count}`
+  forLoopDiv.append(sumElement)
 }
+submitBtn.addEventListener(`click`, addingLoop)
