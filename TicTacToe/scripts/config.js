cdd1d9eb70f9491623e76e427a0e58d97dc64800
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-07 16:47:01 -0600

    TicTacToe

--- TicTacToe/scripts/config.js
+++ TicTacToe/scripts/config.js
@@ -1,3 +1,4 @@
-const  openPlayerConfig= () => {
-
+const  openPlayerConfig = (e) => {
+    const overlay = document.querySelector(`#overlay`)
+    const backdrop = document.querySelector(`#backdrop`)
 }
