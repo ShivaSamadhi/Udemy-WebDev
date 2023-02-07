26511114752b68b6964ff8aa6a2aaa277bf72f56
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-07 17:00:13 -0600

    TicTacToe

--- TicTacToe/scripts/app.js
+++ TicTacToe/scripts/app.js
@@ -1,13 +1,15 @@
 //DOM Selectors
 const configOverlay = document.querySelector(`#overlay`)
 const configBackdrop = document.querySelector(`#backdrop`)
+const cancelBtn = document.querySelector(`#cancelBtn`)
+const confirmBtn = document.querySelector(`#confirmBtn`)
 
 const editP1Btn = document.querySelector(`#editP1Btn`)
 const editP2Btn = document.querySelector(`#editP2Btn`)
 
 //Event Handlers
-configBackdrop.addEventListener(`click`)
-
+configBackdrop.addEventListener(`click`, closePlayerConfig)
+cancelBtn.addEventListener(`click`, closePlayerConfig)
 
 editP1Btn.addEventListener(`click`, openPlayerConfig)
 editP2Btn.addEventListener(`click`, openPlayerConfig)
--- TicTacToe/scripts/config.js
+++ TicTacToe/scripts/config.js
@@ -1,6 +1,9 @@
 const  openPlayerConfig = (e) => {
     configOverlay.style.display = `block`
     configBackdrop.style.display = `block`
+}
 
-
+const closePlayerConfig = () => {
+    configOverlay.style.display = `none`
+    configBackdrop.style.display = `none`
 }
