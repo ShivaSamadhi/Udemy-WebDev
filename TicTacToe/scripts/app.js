ea8407b121331231df5a3a8c59891003067d9ae4
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-07 16:43:27 -0600

    TicTacToe

--- TicTacToe/scripts/app.js
+++ TicTacToe/scripts/app.js
@@ -1,2 +1,5 @@
 const editP1Btn = document.querySelector(`#editP1Btn`)
 const editP2Btn = document.querySelector(`#editP2Btn`)
+
+editP1Btn.addEventListener(`click`, openPlayerConfig)
+editP2Btn.addEventListener(`click`, openPlayerConfig)
