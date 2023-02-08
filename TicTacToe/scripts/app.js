da7b7edf9bc859a3e4b46279635e335211cf9649
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-08 11:56:21 -0600

    TicTacToe

--- TicTacToe/scripts/app.js
+++ TicTacToe/scripts/app.js
@@ -20,6 +20,7 @@ const playerNameDiv = document.querySelector(`#playerNameDiv`)
 const configErr = document.querySelector(`#configErr`)
 const playerName = document.querySelector(`#playerName`)
 const gameField = document.querySelector(`#gameField`)
+let activePlayerName = document.querySelector(`#activePlayerName`)
 
 
 
@@ -28,6 +29,7 @@ const confirmBtn = document.querySelector(`#confirmBtn`)
 const editP1Btn = document.querySelector(`#editP1Btn`)
 const editP2Btn = document.querySelector(`#editP2Btn`)
 const startGameBtn = document.querySelector(`#startGameBtn`)
+const gameBoardBtns = document.querySelectorAll(`#gameBoard li`)
 
 //Event Handlers
 configBackdrop.addEventListener(`click`, closePlayerConfig)
@@ -39,3 +41,7 @@ editP2Btn.addEventListener(`click`, openPlayerConfig)
 configForm.addEventListener(`submit`, savePlayerConfig)
 
 startGameBtn.addEventListener(`click`, startGame)
+
+for (const btn of gameBoardBtns) {
+    btn.addEventListener(`click`, addPlayerSymbol)
+}
--- TicTacToe/scripts/game.js
+++ TicTacToe/scripts/game.js
@@ -1,6 +1,34 @@
 const startGame = () => {
+
+    for (const btn of gameBoardBtns) {
+        btn.classList.remove(`disabled`)
+        btn.textContent = ``
+        btn.addEventListener(`click`, addPlayerSymbol)
+    }
+
   if(players[0].name === `` || players[1].name === ``)
       return alert(`Enter Valid Names For Both Players`)
 
-  gameField.style.display = `block`
+  activePlayerName.textContent = players[0].name
+
+  gameField.style.display = `block`;
+}
+
+const addPlayerSymbol = (e) => {
+    let eTarget = e.target
+    console.log(eTarget)
+    switch (activePlayerName.textContent) {
+        case `${players[0].name}`:
+            eTarget.textContent = `${players[0].symbol}`
+            eTarget.classList.add(`disabled`)
+            activePlayerName.textContent = players[1].name
+            break;
+
+        case `${players[1].name}`:
+            eTarget.textContent = `${players[1].symbol}`
+            eTarget.classList.add(`disabled`)
+            activePlayerName.textContent = players[0].name
+            break;
+    }
+
 }
