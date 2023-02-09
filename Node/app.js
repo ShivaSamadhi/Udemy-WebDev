d9910e44c6a06520516e68f30ad75188a7acda23
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-09 13:47:32 -0600

    Node Express Review

--- Node/app.js
+++ Node/app.js
@@ -3,10 +3,11 @@
 //3.Require it in app.jss
 //4.Call the express function and store it in const app
 //5.Set up port listener, move to bottom of file
-//6.Install & require body-parser
+//6.Install/require fs, path, body-parser
 //7."app.use(bodyParser.urlencoded({extended: true}))"
 //  (can also be used with app.use(express.urlencoded({extended: true}))
 const fs = require(`fs`)
+const path = require(`path`)
 const express = require(`express`)
 const https = require(`https`)
 const bodyParser = require(`body-parser`)
@@ -29,6 +30,10 @@ app.post(`/stored-user`, (req, res)=>{
     const request = req.body
     const {enteredName} = request
     console.log(enteredName)
+
+    const filePath = path.join(__dirname,`data`,`users.json`)
+    fs.writeFileSync(filePath, )
+
     res.send(`
         <h1> Success! ${enteredName} was added </h1>
     `)
