6e7f7502aef529540c3b9a8d1cf75c3ae6d60412
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-10 12:42:17 -0600

    Node Express Review

--- Restaurant/app.js
+++ Restaurant/app.js
@@ -6,13 +6,24 @@ const app = express();
 
 app.set(`view engine`, `ejs`)
 
-app.use(express.static('frontend'))
+app.use(express.static('views'))
 
 
 app.get(`/`, (req, res)=>{
-    res.send(`
-    <h1>Hello World</h1>
-    `)
+    res.sendFile(`${__dirname}/views/index.html`)
+})
+
+app.get(`/restaurants`, (req, res)=>{
+res.sendFile(`${__dirname}/views/restaurants.html`)
+})
+app.get(`/aboutus`, (req, res)=>{
+    res.sendFile(`${__dirname}/views/about.html`)
+})
+app.get(`/recommendations`, (req, res)=>{
+    res.sendFile(`${__dirname}/views/recommend.html`)
+})
+app.get(`/confirm`, (req, res)=>{
+    res.sendFile(`${__dirname}/views/confirm.html`)
 })
 
 app.listen(8080, () => {
