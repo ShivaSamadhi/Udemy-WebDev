132b2161135654afd85278da6f721cadc9f0ecf2
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-10 13:10:58 -0600

    Node Express Review

--- Restaurant/app.js
+++ Restaurant/app.js
@@ -1,13 +1,14 @@
 const express = require(`express`);
 const https = require('https');
 const bodyParser = require(`body-parser`);
+const fs = require("fs");
 
 const app = express();
 
 app.set(`view engine`, `ejs`)
 
 app.use(express.static('public'))
-
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.get(`/`, (req, res)=>{
     res.sendFile(`${__dirname}/views/index.html`)
@@ -16,11 +17,29 @@ app.get(`/`, (req, res)=>{
 app.get(`/restaurants`, (req, res)=>{
 res.sendFile(`${__dirname}/views/restaurants.html`)
 })
+
 app.get(`/aboutus`, (req, res)=>{
     res.sendFile(`${__dirname}/views/about.html`)
 })
+
 app.get(`/recommendations`, (req, res)=>{
     res.sendFile(`${__dirname}/views/recommend.html`)
+})
+app.post(`/recommendations`, (req, res)=>{
+    const restaurant = req.body
+    //Construct absolute file path
+    const filePath = `${__dirname}/data/restaurants.json`
+
+    //Read file data stored at absolute path as raw text
+    const fileData = fs.readFileSync(filePath)
+    //Parse file data into JSON format
+    const storedRestaurants = JSON.parse(fileData)
+    //Push entered name into array stored in file
+    storedRestaurants.push(restaurant)
+    //Convert JSON back to raw text
+    const newRestaurantAdded = JSON.stringify(storedRestaurants)
+    //Write raw data into file
+    fs.writeFileSync(filePath, newRestaurantAdded)
 })
 app.get(`/confirm`, (req, res)=>{
     res.sendFile(`${__dirname}/views/confirm.html`)
