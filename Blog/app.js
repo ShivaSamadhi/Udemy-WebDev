71463d237b5d88b8911c9fad74c6586a11aa70a2
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-14 10:22:50 -0600

    Blog Project

--- Blog/app.js
+++ Blog/app.js
@@ -29,6 +29,6 @@ app.use(express.static("public"));
 
 
 
-app.listen(3000, function() {
-  console.log("Server started on port 3000");
+app.listen(8080, function() {
+  console.log("Server started on port 8080");
 });
