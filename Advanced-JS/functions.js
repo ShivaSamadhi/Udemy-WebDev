dbfa7176e5f59f66f1ad8d043c04ef2b02f529bb
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-23 17:35:28 -0600

    Advanced JS concepts

--- Advanced-JS/functions.js
+++ Advanced-JS/functions.js
@@ -18,7 +18,12 @@ const sumUp = (...numbers) => {
     console.log(sum)
   return sum
 }
-//Using the spread operator within the parameter initialization allows the function to be more flexible because any amount of parameters can be passed to the function call
+//Using the rest/spread operator (which creates an array behind the scenes) within the parameter initialization allows the function to be more flexible because any amount of parameters can be passed to the function call
 
 sumUp(3, 6, 9)
 sumUp(1, -1, -20)
+
+const numArr = [1,2,3,4,5,6]
+sumUp(...numArr)
+//Because the rest/spread operator creates an array behind the scenes, when we place it into the parameter statement it expects a list of values when the function is called.
+//If I pass an array into the function call, it wont work properly. To fix this, we simply use the spread operator on that array within the function call which will effectively turn the array back into a list before executing the function
