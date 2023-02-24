96a622af6cd4aef656d274ca4aeaeeffe3df77e4
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-23 19:11:58 -0600

    Advanced JS concepts

---
+++ Advanced-JS/class.js
@@ -0,0 +1,15 @@
+//classes are templates for creating objects
+//objects are instantiations of classes
+
+class Job {
+    //when a new Job object is created, the constructor func is used to build the necessary fields of that object using the 'this' keyword
+    constructor(title,location, salary) {
+        this.title = title;
+        this.location = location;
+        this.salary = salary;
+    }
+}
+
+const developer = new Job(`Web Developer`, `Remote`, 100000)
+console.log(developer)
+//Using the 'new' keyword creates a new instance of the Job class based on the given parameters
