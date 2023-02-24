549ffa7cb2828c69def9ef7b26a43dcd2acac792
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-23 18:25:47 -0600

    Advanced JS concepts

--- Advanced-JS/functions.js
+++ Advanced-JS/functions.js
@@ -27,3 +27,14 @@ const numArr = [1,2,3,4,5,6]
 sumUp(...numArr)
 //Because the rest/spread operator creates an array behind the scenes, when we place it into the parameter statement it expects a list of values when the function is called.
 //If I pass an array into the function call, it wont work properly. To fix this, we simply use the spread operator on that array within the function call which will effectively turn the array back into a list before executing the function
+//Spread operator can also be used on objects to create a copy of the object which can be manipulated independently of the original
+
+const person = {
+  age: 32
+}
+
+const adultYears = (person) => {
+  person.age -= 18
+    return person.age
+}
+console.log(adultYears({...person}))
