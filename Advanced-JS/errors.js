fb24a54d6661ab7d7d4197474324448bdf57c6c1
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-23 18:56:46 -0600

    Advanced JS concepts

--- Advanced-JS/errors.js
+++ Advanced-JS/errors.js
@@ -6,8 +6,9 @@ const readFile = () => {
     const fileData = fs.readFileSync(`data.json`)
     //This code will fail because data.json doesnt exist. W/o the try catch, any code after this line would not execute
   }
-  catch {
+  catch (e) {
     console.log(`An error occurred!`)
+    console.log(e.message)
   }
 
   console.log(`Hi there`)
