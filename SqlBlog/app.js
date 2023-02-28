f4466c5c16828b34d9a53dad175cc3d5719dbce2
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-28 15:53:48 -0600

    Sql Blog

--- SqlBlog/app.js
+++ SqlBlog/app.js
@@ -3,5 +3,29 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 
-const defaultRouter = require(`./routes/default`)
-const postsRouter = require(`./routes/posts`)
+const defaultRouter = require(`./routes/default`);
+const postsRouter = require(`./routes/posts`);
+
+const app = express();
+
+app.set(`views`, `${__dirname}/views`)
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.use(`/`, defaultRouter)
+app.use(`/`, postsRouter)
+
+app.use((req, res)=>{
+    res.status(404).render(`404`)
+})
+
+app.use((err, req, res, next) => {
+    res.status(500).render(`500`)
+})
+
+
+app.listen(8080, function() {
+    console.log("Server started on port 8080");
+});
