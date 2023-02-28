83f6de1650e9658dd2804aaaa5f68cab3b615d05
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-28 14:06:19 -0600

    Databases

--- Databases/blog_db.sql
+++ Databases/blog_db.sql
@@ -1,2 +1,11 @@
 CREATE SCHEMA blog_db;
 
+create table authors
+(
+    id    int auto_increment,
+    name  varchar(255) not null,
+    email varchar(255) not null,
+    constraint authors_pk
+        primary key (id)
+);
+
