95352fb4cb8c04b39bdeb19d0e0c64b3580c6ddd
Ramaj Johnson <rjohnson3795@gmail.com>
2023-02-28 15:52:15 -0600

    Sql Blog

--- SqlBlog/app.js
+++ SqlBlog/app.js
@@ -2,3 +2,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+
+const defaultRouter = require(`./routes/default`)
+const postsRouter = require(`./routes/posts`)
