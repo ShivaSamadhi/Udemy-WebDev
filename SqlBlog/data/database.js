7a85563306c9585c53bd072ccc4729029c8883af
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-01 09:57:10 -0600

    DB Connection Fixed

--- SqlBlog/data/database.js
+++ SqlBlog/data/database.js
@@ -1,12 +1,12 @@
-const mysql = require(`mysql2/promise`)
+const mysql = require(`mysql2`)
 //mysql2 package allows the server to access the DB
 
 const pool = mysql.createPool({
-    host: `localhost`,
-    database: `blog_db`,
+    host: `127.0.0.1`,
     user: `root`,
-    password: `codeup`
-});
+    database: `blog_db`,
+    password: 'codeup'
+}).promise();
 //createPool() takes in a JS obj that cna be used to configure a connection to the DB
 //Just a quick test
 
--- SqlBlog/routes/blogRoutes.js
+++ SqlBlog/routes/blogRoutes.js
@@ -1,7 +1,7 @@
 "use strict"
 
 const express = require(`express`);
-const {pool} = require(`../data/database`)
+const db = require(`../data/database`)
 const router = express.Router();
 
 
@@ -12,7 +12,7 @@ router.get(`/posts`, (req, res) => {
     res.render(`posts-list`)
 })
  router.get(`/new-post`, async (req, res) => {
-     const [authors] = await pool.query(`Select * From authors`)
+     const [authors] = await db.query(`Select * From authors`)
      //Always returns an arr where the first index is the queried data
      res.render(`create-post`, {authors: authors})
  })
