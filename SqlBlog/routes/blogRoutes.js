7c83e6c103e0c86195eb0dc3376acd03d2a8224a
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-02 11:46:01 -0600

    Databases

--- SqlBlog/routes/blogRoutes.js
+++ SqlBlog/routes/blogRoutes.js
@@ -8,7 +8,9 @@ const router = express.Router();
 router.get(`/`, (req, res) => {
     res.redirect(`/posts`)
 })
-router.get(`/posts`, (req, res) => {
+router.get(`/posts`, async (req, res) => {
+    const [posts] = await db.query(`Select * From posts`)
+
     res.render(`posts-list`)
 })
  router.get(`/new-post`, async (req, res) => {
@@ -19,10 +21,17 @@ router.get(`/posts`, (req, res) => {
  })
 
 router.post(`/posts`, async (req, res) => {
-    const newPost = req.body
+    const newPost = [
+        req.body.title,
+        req.body.summ,
+        req.body.content,
+        req.body.author
+    ]
     await db.query(`
                 Insert Into posts (title, summary, body, author_id) 
-                Values (?, ?, ?, ?)`, [])
+                Values (?)`, [newPost])
+
+    res.redirect(`/posts`)
 })
 
 module.exports = router;
