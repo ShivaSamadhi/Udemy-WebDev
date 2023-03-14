8f87f8594fdae26712f3da89d851b497b0d978a2
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-14 11:47:03 -0500

    MongoDB Blog

--- MongoBlog/app.js
+++ MongoBlog/app.js
@@ -1,19 +1,19 @@
-const path = require('path');
+"use strict"
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
-const express = require('express');
-
-const blogRoutes = require('./routes/blog');
+const blogRoutes = require(`./routes/blogRoutes`)
 
 const app = express();
 
-// Activate EJS view engine
+app.set(`views`, `${__dirname}/views`)
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
 
-app.use(express.urlencoded({ extended: true })); // Parse incoming request bodies
-app.use(express.static('public')); // Serve static files (e.g. CSS files)
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
-app.use(blogRoutes);
+app.use(blogRoutes)
 
 app.use(function (error, req, res, next) {
   // Default error handling function
