daea6e80d2af817bcaffacbb64206b2c802a4be6
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-14 12:39:43 -0500

    MongoDB Blog

--- MongoBlog/data/database.js
+++ MongoBlog/data/database.js
@@ -1,10 +1,11 @@
 const mongodb = require(`mongodb`);
+const data = require("bootstrap/js/src/dom/data");
 const mongoClient = mongodb.MongoClient;
 //sets up the client that we will need to establish a connection
 
 let database
 
-const connect = async () => {
+const connectDB = async () => {
   const client = await mongoClient.connect(`mongodb://localhost:27017`)
   //localhost:27017 is the url port specified in the mongodb docs that connects to the mongodb server
 
@@ -14,6 +15,15 @@ const connect = async () => {
 //the connect method is a promise so placing it in an async function allows us to handle the returned promise
 
 const getDB = () => {
+  if (!database)
+    throw {message: `No DB Connection Established!`}
+  //DB connection check
+
   return database
   //this function returns the data stored within the collection specified in the connect function
+}
+
+module.exports = {
+  connectDB: connectDB,
+  getDB: getDB
 }
