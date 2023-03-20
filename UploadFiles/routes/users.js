af0082f54fff6dc23b08699a1d12dd87f35bdc1d
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-20 14:29:08 -0500

    Uploading files

--- UploadFiles/routes/users.js
+++ UploadFiles/routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const multer = require(`multer`)
 // Middleware for handling multipart/form-data, which is primarily used for uploading files.
+
 const upload = multer({ dest: `images`})
+// multer() takes an obj as a arg that allows you to set the destination of uploaded files, which should be within the FS itself and not the DB
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -12,9 +14,17 @@ router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
 
-router.post(`/profile`, upload.single(`image`), (req, res) => {
-const uploadedImg = req.file;
-const userData = req.body;
+router.post(`/profiles`, upload.single(`image`), (req, res) => {
+  const uploadedImg = req.file;
+  const userData = req.body;
+
+  const {path} = uploadedImg
+
+  console.log(uploadedImg)
+  console.log(userData)
+  console.log(path)
+
+  res.redirect(`/`)
 })
 
 module.exports = router;
