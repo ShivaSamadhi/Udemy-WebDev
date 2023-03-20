dea48057af6371dd3b7c119c956e4dc152a31deb
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-20 15:01:35 -0500

    Uploading files

--- UploadFiles/routes/users.js
+++ UploadFiles/routes/users.js
@@ -1,18 +1,20 @@
 const express = require('express');
+const db = require(`../data/database`)
+
 const multer = require(`multer`)
 // Middleware for handling multipart/form-data, which is primarily used for uploading files.
-
-const db = require(`../data/database`)
 
 const storageConfig = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, `images`);
+  //cb is a callback function given to us by multer, which expects 2 values. The first is a potential err, the second is the destination path or filename.
   },
   filename: (req, file, cb) => {
     cb(null, `${Date.now()}-${file.originalname}`)
   }
 })
-// here we will define an entire storage obj with multer.diskStorage that can be used as the config obj
+// here we will define an entire storage obj with multer.diskStorage that can be used as the config obj for multer()
+// both destination and filename keys require a function as a value that will receive the incoming request, the file, and a callback()
 
 const upload = multer({ storage: storageConfig})
 // multer() takes a config obj as a arg that allows you to tell multer exactly where and how the file should be stored, which should be within the FS itself and not the DB
