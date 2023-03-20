54274ce451c99428d175c06cda160a3f9eb4d430
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-20 13:53:40 -0500

    Uploading files

--- UploadFiles/routes/users.js
+++ UploadFiles/routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
-
+const multer = require(`multer`)
+// Middleware for handling multipart/form-data, which is primarily used for uploading files.
+const upload = multer({})
 const router = express.Router();
 
 router.get('/', function(req, res) {
@@ -9,5 +11,10 @@ router.get('/', function(req, res) {
 router.get('/new-user', function(req, res) {
   res.render('new-user');
 });
+
+router.post(`/profile`, upload.single(`image`), (req, res) => {
+const uploadedImg = req.file;
+const userData = req.body;
+})
 
 module.exports = router;
