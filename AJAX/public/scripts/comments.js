cf26cb055647d7153132fd3b8083e2a6cf104394
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-22 14:23:35 -0500

    AJAX and Fetch Requests

--- AJAX/public/scripts/comments.js
+++ AJAX/public/scripts/comments.js
@@ -1,14 +1,12 @@
 const loadCommentsBtn = document.querySelector(`#loadCommentsBtn`)
 
 const getComments = async () => {
-  const postId = loadCommentsBtn.dataset.postId;
-  const res = await
-      fetch(`/posts/${postId}/comments`)
-      .then(async (res) => await res.json())
-      .then(data => { console.log(data) })
-      .catch((err) => {
-          console.log(`Rejected`, err)
-      })
+  const postId = loadCommentsBtn.dataset.postid;
+  const res = await fetch(`/posts/${postId}/comments`);
+  const resData = await res.json()
+
+  console.log(resData)
+
 }
 
 
