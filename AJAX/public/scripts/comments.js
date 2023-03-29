f4630616bc221b980eb2b434f7fd392f6ac003c0
Ramaj Johnson <rjohnson3795@gmail.com>
2023-03-29 15:49:53 -0500

    AJAX and Fetch Requests

--- AJAX/public/scripts/comments.js
+++ AJAX/public/scripts/comments.js
@@ -2,8 +2,13 @@ const loadCommentsBtn = document.querySelector(`#loadCommentsBtn`)
 
 const getComments = async () => {
   const postId = loadCommentsBtn.dataset.postid;
+  //using the dataset method, we can access the data attr connected to the selected DOM element
+
   const res = await fetch(`/posts/${postId}/comments`);
+  //fetch() handles the async http request to get data from the server
+
   const resData = await res.json()
+  //parses the JSON response from the fetch request
 
   console.log(resData)
 
