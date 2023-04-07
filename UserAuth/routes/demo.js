f7185103108461445e72dab7574193df819d53de
Ramaj Johnson <rjohnson3795@gmail.com>
2023-04-07 14:24:03 -0500

    User Auth

--- UserAuth/routes/demo.js
+++ UserAuth/routes/demo.js
@@ -1,5 +1,5 @@
 const express = require(`express`);
-
+const
 const db = require(`../data/database`);
 
 const router = express.Router();
@@ -22,7 +22,23 @@ router.get(`/admin`, (req, res) => {
 });
 
 //POST ROUTES
-router.post(`/signup`, async (req, res) => {});
+router.post(`/signup`, async (req, res) => {
+  const userData = req.body
+  //Access req body from form submission
+
+  const email = userData.email
+  const confirmEmail = userData.confirm
+  const password = userData.password
+
+  const newUser = {
+    email: email,
+    password: password
+  }
+
+  await db.getDb().collection(`users`).insertOne(newUser)
+
+  res.redirect(`/login`)
+});
 
 router.post(`/login`, async (req, res) => {});
 
