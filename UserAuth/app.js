c162b6462d532735e0cd14b9c74e87eb4c6b3759
Ramaj Johnson <rjohnson3795@gmail.com>
2023-04-07 13:46:53 -0500

    User Auth

--- UserAuth/app.js
+++ UserAuth/app.js
@@ -1,24 +1,29 @@
-const path = require('path');
-
 const express = require('express');
+const ejs = require(`ejs`)
+const bodyParser = require(`body-parser`)
+
 
 const db = require('./data/database');
 const demoRoutes = require('./routes/demo');
 
 const app = express();
 
+
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', `${__dirname}/views`);
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json())
 app.use(express.static('public'));
-app.use(express.urlencoded({ extended: false }));
-
 app.use(demoRoutes);
 
-app.use(function(error, req, res, next) {
-  res.render('500');
-})
+app.use((error, req, res, next) => {
+  console.log(error);
+  res.status(500).render('500');
+});
 
-db.connectToDatabase().then(function () {
-  app.listen(3000);
+db.connectDB().then(() => {
+  app.listen(8080, () => {
+    console.log("Server Started: Port 8080")
+  })
 });
