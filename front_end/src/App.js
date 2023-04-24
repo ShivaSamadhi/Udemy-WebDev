394fa2b6a7af16e06e1a85eaa763fb19bc3ab954
Ramaj Johnson <rjohnson3795@gmail.com>
2023-04-24 15:49:22 -0500

    React App

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+//useState Hook
+//State is a JS obj that contains data about the "current state" of a component. Each comp manages its own state
 import './App.css';
 
 const Person = (props) => {
@@ -9,9 +12,14 @@ const Person = (props) => {
     )
 }
 // ^Functional Component (w/ props)
-//Props function like objects that can be passed as a parameter to functional components. This allows for the dynamic use of key-values when rendering. Here we initialize the properties we want (name, age) when we create the component
+//Props function like an object that can be passed as a parameter to functional components. This allows for the dynamic use of key-values when rendering. Here we initialize the properties we want (name, age) when we create the component
 
 const App = () => {
+  const [counter, setCounter] = useState(0)
+  //State uses array destructuring for implementation of the useState()
+  //The first index represents the name of the state, the second is a setter function.
+  //we can set the initial state by passing it as a parameter to the useState()
+
   const name = {
       firstName: `Ramaj`,
       lastName: `Johnson`
@@ -23,9 +31,13 @@ const App = () => {
       <Person
           name="Ramaj"
           age='28' />
+
+        <button>+</button>
+        <h1>0</h1>
+        <button>-</button>
     </div>
   );
-  //Here we actually define the value of the keys (name and age) when we inject the component
+  //Here we actually define the value of the keys (name and age) as attr when we inject the component
 }
 
 export default App;
