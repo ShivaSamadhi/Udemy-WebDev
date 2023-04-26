15eda531c331dcbb7781a2899d280dd960033cae
Ramaj Johnson <rjohnson3795@gmail.com>
2023-04-26 15:27:45 -0500

    React

---
+++ react_movie/server.js
@@ -0,0 +1,28 @@
+const express = require(`express`)
+const cors = require(`cors`)
+const axios = require(`axios`)
+require(`dotenv`).config()
+
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+const API_KEY = process.env.REACT_APP_API_KEY
+
+app.get(`/:title`, async (req, res) => {
+    const title = req.params.title
+
+    const response = await fetch(`${API_KEY}&s=${title}`)
+    const data = await response.json()
+    const moviesArr = data.Search
+
+    res.json(moviesArr)
+
+})
+
+
+
+app.listen(8080, () => {
+    console.log(`Server Started: Port 8080`)
+})
---
+++ react_movie/src/App.js
@@ -0,0 +1,79 @@
+import React, {useState, useEffect} from "react";
+import './App.css'
+import SearchIcon from './search.svg'
+import MovieCard from "./MovieCard";
+
+
+const localServer = `http://localhost:8080/`
+
+const App = () => {
+
+    //Set State
+    const [movies, setMovies] = useState([])
+    const [searchTerm, setSearchTerm] = useState('')
+
+    //API Search Request
+    // const searchMovies = async (title) => {
+    //     const res = await fetch(`${API_KEY}&s=${title}`)
+    //     const data = await res.json()
+    //     const moviesArr = data.Search
+    //
+    //     setMovies(moviesArr)
+    // }
+
+    const searchMovies = async (title) => {
+        const localServerReq = `http://localhost:8080/${title}`
+        try {
+            const res = await fetch(`${localServerReq}`)
+
+            const resData = await res.json()
+
+            console.log(resData)
+
+            setMovies(resData)
+        }
+        catch{}
+    }
+
+    //On Load useEffect
+    useEffect(() => {
+
+    }, [])
+
+    //Rendered View
+    return (
+        <div className="app">
+            <h1>Reel Sirius</h1>
+
+            <div className="search">
+                <input
+                    type="text"
+                    placeholder="Find Your Movies"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <img
+                    src={SearchIcon}
+                    alt="search"
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+
+            {movies?.length > 0
+                    ? (
+                    <div className="container">
+                        {movies.map((movie) => (
+                            <MovieCard
+                                movie={movie} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className="empty">
+                        <h2>No Movies Found</h2>
+                    </div>
+                )}
+        </div>
+    );
+}
+
+export default App
