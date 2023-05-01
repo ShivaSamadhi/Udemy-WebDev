131331ea4ca9294bfe66ca16e9361dc57edd5080
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-01 11:09:51 -0500

    MERN Auth

--- mern_socialapp/server/index.js
+++ mern_socialapp/server/index.js
@@ -8,6 +8,7 @@ import helmet from "helmet";
 import morgan from "morgan"
 import path from "path";
 import {fileURLToPath} from "url"
+import { register } from "./controllers/auth.js"
 
 //CONFIG
 const __filename = fileURLToPath(import.meta.url)
@@ -36,6 +37,9 @@ const storage = multer.diskStorage({
     },
 })
 const upload = multer({ storage })
+
+//ROUTES
+app.post(`/auth/register`, upload.single(`picture`), register)
 
 //MONGOOSE
 const PORT = process.env.PORT || 8000
