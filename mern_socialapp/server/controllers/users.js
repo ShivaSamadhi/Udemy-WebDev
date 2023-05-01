980fc0af0c8423463f0ba75c03b6d0d18304c57b
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-01 15:52:38 -0500

    MERN Routes/Models/Controllers

--- mern_socialapp/server/controllers/users.js
+++ mern_socialapp/server/controllers/users.js
@@ -21,17 +21,60 @@ export const getUserFriends = async (req, res) => {
         const friends = await Promise.all(
             user.friends.map((id) => User.findById(id))
         )
-        //Map over the friends arr within the user obj, find all friends based on ID, return all as promise
+        //Map over the friends arr within the user obj, find all friends based on ID, return all as promise arr
 
         const formattedFriends = friends.map(
             ({ _id, firstName, lastName, occupation, location, picturePath }) => {
             return { _id, firstName, lastName, occupation, location, picturePath }
         })
-        //Map over and destructure all users in friends obj and return the new obj
+        //Map over and destructure all users in friends arr and return the new obj
 
         res.status(200).json(formattedFriends)
     }
     catch (err) {
         res.status(404).json({ message: err.message})
     }
+}
+
+//UPDATE
+export const addRemoveFriend = async (req, res) => {
+    try{
+        const {id, friendId} = req.params
+
+        const user = await User.findById(id)
+        const friend = await User.findById(friendId)
+
+        if(user.friends.includes(friendId)){
+            user.friends = user.friends.filter((id) => id !== friendId)
+            friend.friends = friend.friends.filter((id) => id !== id)
+            //If friendId exists in user friends list, remove corresponding ID from each users friend arr with filter
+        }
+        else{
+            user.friends.push(friendId)
+            friend.friends.push(id)
+            //If friendId does not exists in user friends list, add corresponding ID to each users friend arr with push
+        }
+
+        await user.save()
+        await friend.save()
+        //Save updated objects
+
+        const friends = await Promise.all(
+            user.friends.map((id) => User.findById(id))
+        )
+        //Map over the friends arr within the user obj, find all friends based on ID, return all as promise arr
+
+        const formattedFriends = friends.map(
+            ({ _id, firstName, lastName, occupation, location, picturePath }) => {
+                return { _id, firstName, lastName, occupation, location, picturePath }
+            })
+        //Map over and destructure all users in friends arr and return the new obj
+
+        res.status(200).json(formattedFriends)
+    }
+    catch (err) {
+        res.status(404).json({ message: err.message})
+    }
+
+
 }
--- mern_socialapp/server/models/Post.js
+++ mern_socialapp/server/models/Post.js
@@ -2,14 +2,35 @@ import mongoose from "mongoose";
 
 const PostSchema = new mongoose.Schema(
     {
-        userID:{},
-        firstName:{},
-        lastName: {},
-        location: {},
-        description: {},
-        userPicturePath: {},
-        picturePath: {},
-        likes: {},
-        comments: {}
-    }
+        userID:{
+          type: String,
+          required: true
+        },
+        firstName:{
+          type: String,
+          required: true
+        },
+        lastName:{
+          type: String,
+          required: true
+        },
+        location: String,
+        description: String,
+        userPicturePath: String,
+        picturePath: String,
+        likes: {
+          type: Map,
+          of: Boolean
+        },
+        comments: {
+          types: Array,
+          default: []
+        }
+    },
+    {timestamps: true}
 )
+
+//DB MODEL
+const Post = mongoose.model("Post", PostSchema)
+
+export default Post
