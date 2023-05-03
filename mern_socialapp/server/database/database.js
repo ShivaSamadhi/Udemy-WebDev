de082d1e44aefd9f06072af1c9cadbb190ac87ad
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-03 14:35:42 -0500

    MVC Design Refactor

--- mern_socialapp/server/database/database.js
+++ mern_socialapp/server/database/database.js
@@ -3,10 +3,18 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connectDB = () => {
-    t
-    return mongoose.connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
+export const connectDB = async () => {
+    try{
+
+        const client = await mongoose.connect(process.env.MONGO_URL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+
+        return client
+    }
+    catch (err) {
+        console.log(`DB Connection Failed: ${err.message}`)
+    }
+    
 }
--- mern_socialapp/server/index.js
+++ mern_socialapp/server/index.js
@@ -38,16 +38,7 @@ app.use(cors())
 app.use("/assets", express.static(path.join(__dirname, `public/assets`)))
 
 //FILE STORAGE
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//         cb(null, "public/assets")
-//     },
-//     filename: function (req, file, cb) {
-//         cb(null, file.originalname)
-//     },
-// })
 const upload = multer({ storage })
-
 
 //ROUTES w/ FILES
 app.post(`/auth/register`, upload.single(`picture`), register)
