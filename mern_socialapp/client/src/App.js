4bb5af5b8268258251e843b72b5da04ccd9bec4d
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-15 12:00:11 -0500

    Redux Global State

--- mern_socialapp/client/src/App.js
+++ mern_socialapp/client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage/homePage";
+import LoginPage from "scenes/loginPage/loginPage";
+import ProfilePage from "scenes/profilePage/profilePage";
 
 const App = () => {
   return (
     <div className="app">
-
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:usedId" element={<ProfilePage />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
