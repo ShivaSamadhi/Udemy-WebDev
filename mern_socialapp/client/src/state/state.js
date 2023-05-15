ae6939e3a6f53faa5710d4cc6af8439f06447acc
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-15 12:24:51 -0500

    Redux Global State

--- mern_socialapp/client/src/state/state.js
+++ mern_socialapp/client/src/state/state.js
@@ -11,8 +11,37 @@ export const authSlice = createSlice({
     name: "auth",
     initialState,
     reducers: {
-        setMode: (state) => {
+        setMode:
+            (state) => {
             state.mode = (state.mode === "light" ? "dark" : "light")
+        },
+        setLogin:
+            (state, action) => {
+            state.user = action.payload.user
+            state.token = action.payload.token
+        },
+        setLogout:
+            (state) => {
+            state.user = null
+            state.token = null
+        },
+        setFriends:
+            (state, action) => {
+            state.user ? state.user.friends = action.payload.friends : console.log(`User Friends Non-Existent`)
+        },
+        setPosts:
+            (state, action) => {
+            state.posts = action.payload.posts
+        },
+        setPost: (state, action) => {
+            const updatedPosts = state.posts.map(post =>{
+                post._id === action.payload.post_id ? action.payload.post : post
+            })
+            state.posts = updatedPosts
         }
     }
 })
+
+export const { setMode, setLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions
+
+export default authSlice.reducer
