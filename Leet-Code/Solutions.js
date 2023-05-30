77b182e42dc59c91ff410401d53fd472c693ae79
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-30 15:17:40 -0500

    LeetCode

--- Leet-Code/Solutions.js
+++ Leet-Code/Solutions.js
@@ -114,3 +114,29 @@ const reduce = (nums, fn, init) => {
 
     return val
 };
+
+/*2629. Function Composition
+Given an array of functions [f1, f2, f3, ..., fn], return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+*/
+const compose = (functions) => {
+    return x => {
+        let val
+
+        if(functions.length === 0)
+            return x
+
+        for(let i = functions.length-1; i >= 0; i--)
+            (i === functions.length-1) ?
+                val = functions[i](x) :
+                val = functions[i](val)
+
+        return val
+    }
+};
