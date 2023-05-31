4bca1eb3221d29ed81f24a175c03f68a5fa3ad55
Ramaj Johnson <rjohnson3795@gmail.com>
2023-05-31 16:05:42 -0500

    LeetCode

--- Leet-Code/Solutions.js
+++ Leet-Code/Solutions.js
@@ -194,11 +194,26 @@ const memoize = (fn) => {
         if(args in cache)
             return cache[args]
 
-        let result = fn(...args)
+        cache[args] = fn(...args)
 
-        cache[args] = result
-
-        return result
+        return cache[args]
     }
 }
 
+/*2632. Curry
+Given a function fn, return a curried version of that function.
+
+A curried function is a function that accepts fewer or an equal number of parameters as the original function and returns either another curried function or the same value the original function would have returned.
+
+In practical terms, if you called the original function like sum(1,2,3), you would call the curried version like csum(1)(2)(3), csum(1)(2,3), csum(1,2)(3), or csum(1,2,3). All these methods of calling the curried function should return the same value as the original.*/
+const curry = (fn) => {
+    return curried = (...args) => {
+        if(args.length >= fn.length)
+            return fn.apply(this, args)
+
+        return (...args2) => {
+            return curried
+                .apply(this, args.concat(args2))
+        }
+    };
+};
